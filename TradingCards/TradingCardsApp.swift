c89b95c6f710d47de7a0007c24f1ab3bac100fc9
Judy <jyu@lcs.on.ca>
2021-11-06 10:48:36 -0400

    Initial Commit

---
+++ TradingCards/TradingCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradingCardsApp.swift
+//  TradingCards
+//
+//  Created by Judy Yu on 2021-11-06.
+//
+
+import SwiftUI
+
+@main
+struct TradingCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
