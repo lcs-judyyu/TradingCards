be1d2a504c865aa93a64287241688ea171f68dc3
Judy <jyu@lcs.on.ca>
2021-11-08 20:08:57 -0500

    Add background to photo and name section

--- TradingCards/ContentView.swift
+++ TradingCards/ContentView.swift
@@ -13,30 +13,42 @@ struct ContentView: View {
             //background image
             Image("HockeyCardsBackground")
                 .resizable()
+            
             //content
             VStack(alignment: .leading, spacing: 10){
+                //hockey icon
                 Image("HockeyIconWhite")
                     .resizable()
                     .frame(width: 100, height: 100, alignment: .topLeading)
                 HStack(alignment: .top, spacing: 10) {
-                    Text("Ice Hockey")
-                        .textCase(.uppercase)
-                        .rotationEffect(.degrees(-90))
                     VStack {
+                        //ice hockey text
+                    Text("Ice Hockey")
+                        .font(Font.custom("SairaStencilOne-Regular", size: 33))
+                        //.frame(width: 50, height: 300, alignment: .topLeading)
+                        .textCase(.uppercase)
+                        //.rotationEffect(.degrees(-90))
+                    }
+                    VStack {
+                        //photo
                         Image("JackJohnson")
                             .resizable()
                             .frame(width: 300, height: 300, alignment: .trailing)
                         .clipShape(RoundedRectangle(cornerRadius: 9, style: .continuous))
                         VStack {
+                            //name
                         Text("John Johnson")
+                            //team name
                         Text("New York Rangers")
                         }
                         .padding(.bottom, 90)
                     }
+                    .padding(10)
+                    .background()
                 }
                 Text("Data")
                     .padding(.top, 50)
-                    .padding(.bottom, 130)
+                    .padding(.bottom, 140)
             }
         }
         .edgesIgnoringSafeArea(.all)
