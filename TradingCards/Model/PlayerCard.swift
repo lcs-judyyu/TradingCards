71b200e28d97e637fa41a55ad0d483398f7873ab
Judy <jyu@lcs.on.ca>
2021-11-09 20:04:14 -0500

    Create folders and files that will be used later

---
+++ TradingCards/Model/PlayerCard.swift
@@ -0,0 +1,20 @@
+//
+//  PlayerCard.swift
+//  TradingCards
+//
+//  Created by Judy Yu on 2021-11-09.
+//
+
+import SwiftUI
+
+struct PlayerCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PlayerCard_Previews: PreviewProvider {
+    static var previews: some View {
+        PlayerCard()
+    }
+}
